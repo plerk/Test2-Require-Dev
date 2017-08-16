417ce3abb09461289eddc8c8f225691e32a27b14
Graham Ollis <plicease@cpan.org>
2017-08-16 13:50:01 -0400

    Initial commit

---
+++ lib/Test2/Require/Dev.pm
@@ -0,0 +1,10 @@
+package Test2::Require::Dev;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Only run a test in development tree or in dev releases
+# VERSION
+
+1;
---
+++ t/test2_require_dev.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Test2::Require::Dev;
+
+ok 1, 'todo';
+
+done_testing
